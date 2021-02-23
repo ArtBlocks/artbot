4bef0c5159207523049b5faa98e5b266b0d48100
Jacob Rockland <jakerockland@gmail.com>
2021-02-22 23:32:33 -0700

    Migrate all existing curated project bots to use the new shared bot implementation.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
-const { Client, MessageEmbed } = require("discord.js");
+const {
+  Client,
+  MessageEmbed
+} = require("discord.js");
 const Web3 = require("web3");
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -13,7 +16,9 @@ const CHANNEL_SQUIG = process.env.CHANNEL_SQUIG;
 const CHANNEL_RINGERS = process.env.CHANNEL_RINGERS;
 const SERVER = process.env.SERVER;
 
-const { init } = require("./singularity");
+const {
+  init
+} = require("./singularity");
 const os = require("./osEvent");
 const ignition = require("./ignition");
 const squig = require("./squiggle");
@@ -29,21 +34,25 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-app.post("/update", function (req, res) {
+app.post("/update", function(req, res) {
   console.log("received update with body:\n", JSON.stringify(req.body, null, 2), "\n");
 
   res.setHeader("Content-Type", "application/json");
-  res.json({ success: true });
+  res.json({
+    success: true
+  });
 });
 
-app.get("/update", function (req, res) {
+app.get("/update", function(req, res) {
   console.log("received get with body:\n", req.body, "\n");
 
   res.setHeader("Content-Type", "application/json");
-  res.json({ success: true });
+  res.json({
+    success: true
+  });
 });
 
-app.listen(PORT, function () {
+app.listen(PORT, function() {
   console.log("Server is listening on port ", PORT);
 });
 
@@ -84,35 +93,28 @@ async function metaData(data, msg, url) {
     })
 
     .addFields(
-      data.event_type
-        ? data.event_type == "created"
-          ? {
-              name: "Offered At",
-              value: ` ${web3.utils.fromWei(
+      data.event_type ?
+      data.event_type == "created" ? {
+        name: "Offered At",
+        value: ` ${web3.utils.fromWei(
                 data.ending_price,
                 "ether"
               )}Ξ on ${new Date(data.created_date).toLocaleDateString()}`,
-              inline: true,
-            }
-          : data.event_type == "successful"
-          ? {
-              name: "Last Sale",
-              value: `Sold for ${web3.utils.fromWei(
+        inline: true,
+      } :
+      data.event_type == "successful" ? {
+        name: "Last Sale",
+        value: `Sold for ${web3.utils.fromWei(
                 data.total_price,
                 "ether"
               )}Ξ on ${new Date(data.created_date).toLocaleDateString()}`,
-              inline: true,
-            }
-          : data.event_type == "transfer"
-          ? {
-              name:
-                data.from_account.address == mintAddress
-                  ? "Minted On:"
-                  : "Recent Transfer",
-              value:
-                data.from_account.address == mintAddress
-                  ? new Date(data.created_date).toLocaleDateString()
-                  : `From [${data.from_account.address.slice(
+        inline: true,
+      } :
+      data.event_type == "transfer" ? {
+        name: data.from_account.address == mintAddress ?
+          "Minted On:" : "Recent Transfer",
+        value: data.from_account.address == mintAddress ?
+          new Date(data.created_date).toLocaleDateString() : `From [${data.from_account.address.slice(
                       0,
                       8
                     )}](https://opensea.io/accounts/${
@@ -122,12 +124,11 @@ async function metaData(data, msg, url) {
                         ? `(${data.from_account.user.username.slice(0, 10)})`
                         : ""
                     } on ${new Date(data.created_date).toLocaleDateString()}`,
-              inline: true,
-            }
-          : data.event_type == "bid_withdrawn"
-          ? {
-              name: "Bid Withdrawn",
-              value: ` ${web3.utils.fromWei(
+        inline: true,
+      } :
+      data.event_type == "bid_withdrawn" ? {
+        name: "Bid Withdrawn",
+        value: ` ${web3.utils.fromWei(
                 data.total_price,
                 "ether"
               )}Ξ from [${data.from_account.address.slice(
@@ -138,12 +139,11 @@ async function metaData(data, msg, url) {
                   ? `(${data.from_account.user.usernameslice(0, 10)})`
                   : ""
               }  on ${new Date(data.created_date).toLocaleDateString()}`,
-              inline: true,
-            }
-          : data.event_type == "cancelled"
-          ? {
-              name: "Cancelled Offer",
-              value: ` ${web3.utils.fromWei(
+        inline: true,
+      } :
+      data.event_type == "cancelled" ? {
+        name: "Cancelled Offer",
+        value: ` ${web3.utils.fromWei(
                 data.total_price,
                 "ether"
               )}Ξ from [${data.asset.owner.address.slice(
@@ -154,11 +154,10 @@ async function metaData(data, msg, url) {
                   ? `(${data.asset.owner.user.username})`
                   : ""
               }  on ${new Date(data.created_date).toLocaleDateString()}`,
-              inline: true,
-            }
-          : {
-              name: "Current Bid",
-              value: ` ${web3.utils.fromWei(
+        inline: true,
+      } : {
+        name: "Current Bid",
+        value: ` ${web3.utils.fromWei(
                 data.bid_amount,
                 "ether"
               )}Ξ from [${data.from_account.address.slice(
@@ -169,9 +168,11 @@ async function metaData(data, msg, url) {
                   ? `(${data.from_account.user.username})`
                   : ""
               }  on ${new Date(data.created_date).toLocaleDateString()}`,
-              inline: true,
-            }
-        : { name: "Last Sale", value: "No Transactions" }
+        inline: true,
+      } : {
+        name: "Last Sale",
+        value: "No Transactions"
+      }
     );
 
   msg.channel.send(_embed);
@@ -186,13 +187,13 @@ async function singData(msg, number) {
   }
 
   await fetch(
-    `https://api.opensea.io/api/v1/events?asset_contract_address=${_contract}&token_id=${_token}&only_opensea=false&offset=0&limit=5`,
+      `https://api.opensea.io/api/v1/events?asset_contract_address=${_contract}&token_id=${_token}&only_opensea=false&offset=0&limit=5`,
 
-    {
-      method: "GET",
-      headers: {},
-    }
-  )
+      {
+        method: "GET",
+        headers: {},
+      }
+    )
     .then((response) => response.json())
     .then((data) => {
       let event = data.asset_events[0];
@@ -208,26 +209,49 @@ bot.on("ready", () => {
   console.info(`Logged in as ${bot.user.tag}!`);
 });
 
+let singularityBot = new CuratedProjectBot(
+  8000000,
+  "0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270",
+  1024,
+  "Singularity"
+);
+let ignitionBot = new CuratedProjectBot(
+  9000000,
+  "0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270",
+  512,
+  "Ignition"
+);
+let squiggleBot = new CuratedProjectBot(
+  0,
+  "0x059edd72cd353df5106d2b9cc5ab83a52287ac3a",
+  10000,
+  "Chromie Squiggle"
+);
 let ringersBot = new CuratedProjectBot(
   13000000,
   "0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270",
   1000,
   "Ringers"
-)
+);
 
 bot.on("message", (msg) => {
   if (msg.content.startsWith("#")) {
-    if (msg.channel.id === CHANNEL_SING) {
-      singData(msg, msg.content);
-    }
-    if (msg.channel.id === CHANNEL_IGNITION) {
-      ignition.ignitionData(msg, msg.content);
-    }
-    if (msg.channel.id === CHANNEL_SQUIG) {
-      squig.squigData(msg, msg.content);
-    }
-    if (msg.channel.id === CHANNEL_RINGERS) {
-      ringersBot.getData(msg, msg.content);
+    switch (msg.channel.id) {
+      case CHANNEL_SING:
+        singularityBot.getData(msg, msg.content);
+        break;
+      case CHANNEL_IGNITION:
+        ignitionBot.getData(msg, msg.content);
+        break;
+      case CHANNEL_SQUIG:
+        squiggleBot.getData(msg, msg.content);
+        break;
+      case CHANNEL_RINGERS:
+        ringersBot.getData(msg, msg.content);
+        break;
+      default:
+        console.log(`Unknown channel ID: ${msg.channel.id}`);
+        break;
     }
   }
 });
