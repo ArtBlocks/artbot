e37fcf59e0889a8b317f89413fabaa8c7842d0cc
Jacob Rockland <jakerockland@gmail.com>
2021-02-23 22:41:56 -0700

    Code cleanup for index.js

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ const {
   Client,
   MessageEmbed
 } = require("discord.js");
-const Web3 = require("web3");
 const express = require("express");
 const bodyParser = require("body-parser");
-const fetch = require("node-fetch");
-const bot = new Client();
+const os = require("./osEvent");
+const CuratedProjectBot = require("./CuratedProjectBot").CuratedProjectBot;
 
+// Misc. server configuration info.
 const TOKEN = process.env.TOKEN;
 const SERVER = process.env.SERVER;
+const PORT = process.env.PORT || 3000;
 
+// Curated project Discord channel IDs.
 const CHANNEL_SING = process.env.CHANNEL_SING;
 const CHANNEL_TRADE = process.env.CHANNEL_TRADE;
 const CHANNEL_IGNITION = process.env.CHANNEL_IGNITION;
@@ -27,9 +29,11 @@ const CHANNEL_UNIGRIDS = process.env.CHANNEL_UNIGRIDS;
 const CHANNEL_27_BIT = process.env.CHANNEL_27_BIT;
 const CHANNEL_CRYPTOBLOTS = process.env.CHANNEL_CRYPTOBLOTS;
 
+// Minting contract addresses.
 const OG_MINTING_CONTRACT_ADDRESS = "0x059edd72cd353df5106d2b9cc5ab83a52287ac3a";
 const V2_MINTING_CONTRACT_ADDRESS = "0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270";
 
+// Custom message shown when someone asks why Squiggle minting is paused.
 const SQUIGGLE_PAUSE_MESSAGE = new MessageEmbed()
       // Set the title of the field
       .setTitle('Why is minting paused?')
@@ -38,15 +42,8 @@ const SQUIGGLE_PAUSE_MESSAGE = new MessageEmbed()
       // Set the main content of the embed
       .setDescription(`It looks like you're wondering about why Chromie Squiggle minting is paused.The tl;dr is that all normal minting is over and the remaining Squiggles are reserved for special occasions!\n\nFor more details, check out the [#squiggle-announcements](https://discord.com/channels/411959613370400778/800461920008273962/800464186924466187) channel.`);
 
-const os = require("./osEvent");
-const CuratedProjectBot = require("./CuratedProjectBot").CuratedProjectBot;
-
-let web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
-
-bot.login(TOKEN);
-
+// App setup.
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -72,10 +69,16 @@ app.listen(PORT, function() {
   console.log("Server is listening on port ", PORT);
 });
 
+// Bot setup.
+const bot = new Client();
+
+bot.login(TOKEN);
+
 bot.on("ready", () => {
   console.info(`Logged in as ${bot.user.tag}!`);
 });
 
+// Curated project Discord channel message handlers.
 let singularityBot = new CuratedProjectBot(
   8000000,
   V2_MINTING_CONTRACT_ADDRESS,
@@ -155,6 +158,7 @@ let cryptoblotBot = new CuratedProjectBot(
   "Cryptoblots"
 );
 
+// Message event handler.
 bot.on("message", (msg) => {
   // Handle piece # requests.
   if (msg.content.startsWith("#")) {
@@ -216,4 +220,6 @@ bot.on("message", (msg) => {
   }
 });
 
+// Set up OpenSea event listener polling.
+// TODO(jakerockland@): Replace with webhook implementation.
 setInterval(os.openseaEvent, 60 * 1000, bot);
