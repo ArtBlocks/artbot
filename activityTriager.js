ea226b6c03558e6c2cddb4d1d7662dc0fe1ef1af
Jacob Rockland <jakerockland@gmail.com>
2021-03-30 21:25:13 -0600

    Make the messages more condensed.

--- activityTriager.js
+++ activityTriager.js
@@ -14,17 +14,41 @@ async function triageActivityMessage(msg, bot) {
   let embeds = msg.embeds
   for (i = 0; i < embeds.length; i++) {
     let embed = embeds[i];
+
     // Determine the item that the event is associated with.
     let openseaURL = embed.author.url;
     let urlComponents = openseaURL.split("/");
     let tokenID = urlComponents[urlComponents.length - 1];
 
+    // Extract out the "author name".
+    let authorName = embed.author.name;
+    let authorURL = embed.author.url;
+    let eventName = authorName.split(":")[0];
+
+    // Get current description.
+    let description = embed.description;
+
+    // Split off the "Description" text within the description.
+    let descriptionDescriptionIndex = description.indexOf("\n**Description:**");
+    description = description.substring(0, descriptionDescriptionIndex + 1);
+
+    // Assuming that "Name" is the first field, remove it.
+    let nameLineBreakIndex = description.indexOf("\n");
+    let lastIndex = description.length - 1;
+    description = description.substring(nameLineBreakIndex, lastIndex);
+
+    // Update description with parsed and modified string.
+    embed.setDescription(description.trim());
+
     // Get Art Blocks metadata response for the item.
     let artBlocksResponse = await fetch(`https://api.artblocks.io/token/${tokenID}`);
     let artBlocksData = await artBlocksResponse.json();
 
-    // Add field for viewing live script on Art Blocks.
-    embed.addField("Live Script", `[view on artblocks.io](${artBlocksData.external_url})`)
+    // Add inline field for viewing live script on Art Blocks.
+    embed.addField("Live Script", `[view on artblocks.io](${artBlocksData.external_url})`, true);
+
+    // Update embed author name to reflect piece name rather than token number.
+    embed.setAuthor(`${eventName}: ${artBlocksData.name}`, null, authorURL);
 
     // Determine channel from the curation status of the piece.
     // Fall-back to factory as the default (so that no messages accidentally
