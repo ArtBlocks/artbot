ace9289b1d0aee30dd76e61ccfa7eb0418a5900c
Jacob Rockland <jakerockland@gmail.com>
2021-06-03 17:54:05 -0600

    Expand blocklist

--- Utils/activityTriager.js
+++ Utils/activityTriager.js
@@ -16,7 +16,8 @@ const BAN_ADDRESSES = [
     "0x33d27f0bb797de3d20e519f4a192b7570c56681b",
     "0xed30fdda2d9c605ee9c519581d65de65fb58daed",
     "0x9048d24577d4c4bbf14f1020f20640b334f8c762",
-    "0x3c6137504c38215fea30605b3e364a23c1d3e14f"
+    "0x3c6137504c38215fea30605b3e364a23c1d3e14f",
+    "0xbd67097f6ef72f5337cb4932391e04d2d07e1a61"
 ];
 
 async function triageActivityMessage(msg, bot) {
