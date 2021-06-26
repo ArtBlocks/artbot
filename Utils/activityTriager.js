1d072ef8272fba7a0ccc372e45371656f06e8a4d
Jacob Rockland <jakerockland@gmail.com>
2021-06-25 18:31:06 -0600

    expand deny list

--- Utils/activityTriager.js
+++ Utils/activityTriager.js
@@ -19,7 +19,8 @@ const BAN_ADDRESSES = [
     "0x3c6137504c38215fea30605b3e364a23c1d3e14f",
     "0xbd67097f6ef72f5337cb4932391e04d2d07e1a61",
     "0xb1e6f68aa3ab791f2e835d84a9c1c2b054aa3598",
-    "0x2ad7d5ac35319d221b2d1c7ee9edb2e3d106962e"
+    "0x2ad7d5ac35319d221b2d1c7ee9edb2e3d106962e",
+    "0x438681aa97bf5ecf1fe9110d1b04ed8230e2bfad"
 ];
 
 async function triageActivityMessage(msg, bot) {
