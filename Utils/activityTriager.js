1283f1362cbebd2fb892b6f4a73683eec5a720c2
Jacob Rockland <jakerockland@gmail.com>
2021-08-09 23:20:30 -0600

    trimming

--- Utils/activityTriager.js
+++ Utils/activityTriager.js
@@ -118,7 +118,7 @@ async function triageActivityMessage(msg, bot) {
     // Update to remove author name and to reflect this info in piece name
     // rather than token number as the title and URL field..
     embed.author = null;
-    embed.setTitle(`${eventName}: ${artBlocksData.name}`);
+    embed.setTitle(`${artBlocksData.name}`);
     embed.setURL(openseaURL);
 
     // Only forward sales events and listing events.
