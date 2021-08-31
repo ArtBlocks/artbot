c1e5949bda75b02b0b50aafcf154835fdb359e31
Jacob Rockland <jakerockland@gmail.com>
2021-08-31 09:38:43 -0600

    minor

--- Classes/RandomBot.js
+++ Classes/RandomBot.js
@@ -1,6 +1,7 @@
 require("dotenv").config(); const {
   MessageEmbed
 } = require("discord.js");
+
 const fetch = require("node-fetch");
 const Web3 = require("web3");
 const getArtBlocksPlatform = require("../Utils/parseArtBlocksAPI").getArtBlocksPlatform;
@@ -11,7 +12,7 @@ const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
 const V2_MINTING_CONTRACT_ADDRESS = "0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270";
 
 // Refresh takes around one minute, so recommend setting this to 60 minutes
-const METADATA_REFRESH = process.env.METADATA_REFRESH_INTERVAL_MINUTES;
+const METADATA_REFRESH_INTERVAL_MINUTES = process.env.METADATA_REFRESH_INTERVAL_MINUTES;
 
 // This array will hold ProjectBot classes for all the Factory Projects we find
 let projectCount = 0;
@@ -19,19 +20,16 @@ let projectCount = 0;
 class RandomBot {
   constructor() {
     this.initialize();
-    setInterval(this.initialize, METADATA_REFRESH * 60000);
+    setInterval(this.initialize, METADATA_REFRESH_INTERVAL_MINUTES * 60000);
   }
 
   async initialize() {
-
     let projectList = await getArtBlocksPlatform();
     projectCount = projectList[projectList.length-1];
     console.log("Loading project count: ${projectCount}");
-
   }
- 
-  async handleRandomMessage(msg) {
 
+  async handleRandomMessage(msg) {
     let content = msg.content;
 
     if (content.length <= 1) {
@@ -42,11 +40,12 @@ class RandomBot {
     if (afterTheHash[0] != "?") {
       return;
     }
+
     let attempts = 0;
     while (attempts < 10) {
         let projectNumber = parseInt(Math.random() * projectCount);
         console.log("trying to look for project ${projectNumber}");
-        let projectData = await getArtBlocksProject(projectNumber); 
+        let projectData = await getArtBlocksProject(projectNumber);
         if (projectData) {
             let pieceNumber = parseInt(Math.random() * projectData.invocations);
             let tokenID = projectNumber * 1e6 + pieceNumber;
@@ -62,9 +61,9 @@ class RandomBot {
 		    .setImage(artBlocksData.image);
 	    msg.channel.send(imageContent);
 	    return;
-	}
+	 }
         attempts++;
-    }            
+    }
     return;
   }
 }
