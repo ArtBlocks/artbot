1e8642e37ae75d98a5230172fb3728598680c07d
Grant <grantoesterling1@gmail.com>
2022-05-03 13:12:36 -0500

    retriggering build

--- Utils/parseArtBlocksAPI.js
+++ Utils/parseArtBlocksAPI.js
@@ -34,8 +34,8 @@ const contractProject = gql`
         name
         invocations
         maxInvocations
-        curationStatus
         active
+        curationStatus
         contract {
           id
         }
