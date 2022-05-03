c202057d72af8f13f7396b131e19901a9d17eaac
Grant Oesterling <grant@artblocks.io>
2022-05-03 16:02:49 -0500

    Add LooksRare API Polling (#261)
    * Adding rough LooksRare poller
    
    * Commonizing API polling code
    
    * Commonizing channel triage logic
    
    * Code review fixes!
    
    * triggering rebuild
    
    Co-authored-by: Jake Rockland <jake@artblocks.io>

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 require('dotenv').config();
-const {
-  Client,
-  MessageEmbed,
-} = require('discord.js');
+const {Client} = require('discord.js');
 const {GiveawaysManager} = require('discord-giveaways');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -11,11 +8,14 @@ const AddressCollector = require('./Classes/AddressCollector').AddressCollector;
 const FactoryBot = require('./Classes/FactoryBot').FactoryBot;
 const RandomBot = require('./Classes/RandomBot').RandomBot;
 const projectConfig = require('./ProjectConfig/projectConfig').projectConfig;
-
+const CORE_CONTRACTS = require('./ProjectConfig/coreContracts.json');
+const {LooksRareAPIPollBot} = require('./Classes/LooksRareAPIPollBot');
 // Special handlers.
-const triageActivityMessage = require('./Utils/activityTriager').triageActivityMessage;
+const {triageActivityMessage} = require('./Utils/activityTriager');
+
 const smartBotResponse = require('./Utils/smartBotResponse').smartBotResponse;
-const handleGiveawayMessage = require('./Utils/giveawayCommands').handleGiveawayMessage;
+const handleGiveawayMessage =
+  require('./Utils/giveawayCommands').handleGiveawayMessage;
 
 // Misc. server configuration info.
 const TOKEN = process.env.TOKEN;
@@ -33,13 +33,20 @@ const CHANNEL_ART_CHAT = projectConfig.chIdByName['ab-art-chat'];
 // Special address collection channel.
 const CHANNEL_ADDRESS_COLLECTION = process.env.CHANNEL_ADDRESS_COLLECTION;
 
+// Rate (in ms) to poll API endpoints
+const API_POLL_TIME_MS = 20000;
+
 // App setup.
 const app = express();
 
 app.use(bodyParser.json());
 
 app.post('/update', function(req, res) {
-  console.log('received update with body:\n', JSON.stringify(req.body, null, 2), '\n');
+  console.log(
+      'received update with body:\n',
+      JSON.stringify(req.body, null, 2),
+      '\n',
+  );
 
   res.setHeader('Content-Type', 'application/json');
   res.json({
@@ -64,7 +71,7 @@ app.listen(PORT, function() {
 const bot = new Client();
 bot.login(TOKEN);
 
-bot.on('ready', client => {
+bot.on('ready', (client) => {
   console.info(`Logged in as ${bot.user.tag}!`);
   randomGuy.startRoutine(bot.channels.cache.get(CHANNEL_ART_CHAT));
 });
@@ -79,14 +86,28 @@ bot.giveawaysManager = new GiveawaysManager(bot, {
     reaction: '🎉',
   },
 });
-bot.giveawaysManager.on('giveawayReactionAdded', (giveaway, member, reaction) => {
-  console.log(`${member.user.tag} entered giveaway #${giveaway.messageID} (${reaction.emoji.name})`);
-});
-bot.giveawaysManager.on('giveawayReactionRemoved', (giveaway, member, reaction) => {
-  console.log(`${member.user.tag} unreact to giveaway #${giveaway.messageID} (${reaction.emoji.name})`);
-});
+bot.giveawaysManager.on(
+    'giveawayReactionAdded',
+    (giveaway, member, reaction) => {
+      console.log(
+          `${member.user.tag} entered giveaway #${giveaway.messageID} (${reaction.emoji.name})`,
+      );
+    },
+);
+bot.giveawaysManager.on(
+    'giveawayReactionRemoved',
+    (giveaway, member, reaction) => {
+      console.log(
+          `${member.user.tag} unreact to giveaway #${giveaway.messageID} (${reaction.emoji.name})`,
+      );
+    },
+);
 bot.giveawaysManager.on('giveawayEnded', (giveaway, winners) => {
-  console.log(`Giveaway #${giveaway.messageID} ended! Winners: ${winners.map((member) => member.user.username).join(', ')}`);
+  console.log(
+      `Giveaway #${giveaway.messageID} ended! Winners: ${winners
+          .map((member) => member.user.username)
+          .join(', ')}`,
+  );
 });
 
 const factoryParty = new FactoryBot();
@@ -110,18 +131,18 @@ bot.on('message', (msg) => {
   }
 
   /*
-     * If the message is in the activity channel, forward the message on
-     * To the appropriate sub-channel.
-     */
+   * If the message is in the activity channel, forward the message on
+   * To the appropriate sub-channel.
+   */
   if (channelID == PROD_CHANNEL_ACTIVITY_ALL) {
     triageActivityMessage(msg, bot);
     return;
   }
 
   /*
-     * If message is in special address collection channel, forward message
-     * To that handler and return early.
-     */
+   * If message is in special address collection channel, forward message
+   * To that handler and return early.
+   */
   if (channelID == CHANNEL_ADDRESS_COLLECTION) {
     addressCollector.addressCollectionHandler(msg);
     return;
@@ -153,14 +174,46 @@ bot.on('message', (msg) => {
 
   // Handle special info questions that ArtBot knows how to answer.
   const artBotID = bot.user.id;
-  smartBotResponse(msgContentLowercase, msgAuthor, artBotID, channelID).then((smartResponse) => {
-    if (smartResponse !== null && smartResponse !== undefined) {
-      msg.reply(null, {
-        embed: smartResponse,
-        allowedMentions: {
-          repliedUser: true,
-        },
-      });
-    }
-  });
+  smartBotResponse(msgContentLowercase, msgAuthor, artBotID, channelID).then(
+      (smartResponse) => {
+        if (smartResponse !== null && smartResponse !== undefined) {
+          msg.reply(null, {
+            embed: smartResponse,
+            allowedMentions: {
+              repliedUser: true,
+            },
+          });
+        }
+      },
+  );
 });
+
+// Instantiate API Pollers
+
+// LooksRare pollers for V2 Contract
+// List Events
+new LooksRareAPIPollBot(
+    `https://api.looksrare.org/api/v1/events?collection=${CORE_CONTRACTS.V2}&type=LIST&pagination[first]=25`,
+    API_POLL_TIME_MS,
+    bot,
+);
+// Sale Events
+new LooksRareAPIPollBot(
+    `https://api.looksrare.org/api/v1/events?collection=${CORE_CONTRACTS.V2}&type=SALE&pagination[first]=25`,
+    API_POLL_TIME_MS,
+    bot,
+);
+
+// LooksRare pollers for OG Contract
+// List Events
+new LooksRareAPIPollBot(
+    `https://api.looksrare.org/api/v1/events?collection=${CORE_CONTRACTS.OG}&type=LIST&pagination[first]=25`,
+    API_POLL_TIME_MS,
+    bot,
+);
+// Sale Events
+new LooksRareAPIPollBot(
+    `https://api.looksrare.org/api/v1/events?collection=${CORE_CONTRACTS.OG}&type=SALE&pagination[first]=25`,
+    API_POLL_TIME_MS,
+    bot,
+);
