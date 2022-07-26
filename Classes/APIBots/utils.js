f8b14675748875e256c95c4b07f82dad13f616e4
grant <grant@artblocks.io>
2022-07-26 14:44:53 -0400

    Adding OS Username Fetch

--- Classes/APIBots/utils.js
+++ Classes/APIBots/utils.js
@@ -1,18 +1,20 @@
 const ethers = require('ethers')
+const fetch = require('node-fetch')
 
 let provider = new ethers.providers.AlchemyProvider('homestead')
 
 // Runtime ENS cache just to limit queries
-let addressMap = {}
+let ensAddressMap = {}
+let osAddressMap = {}
 
 async function getENSName(address) {
   let name = ''
-  if (addressMap[address]) {
-    name = addressMap[address]
+  if (ensAddressMap[address]) {
+    name = ensAddressMap[address]
   } else {
     let ens = await provider.lookupAddress(address)
     name = ens ?? ''
-    addressMap[address] = name
+    ensAddressMap[address] = name
   }
   return name
 }
@@ -22,4 +24,35 @@ async function ensOrAddress(address) {
   return ens !== '' ? ens : address
 }
 
+async function getOSName(address) {
+  let name = ''
+  if (osAddressMap[address]) {
+    console.log('Cached!')
+    name = osAddressMap[address]
+  } else {
+    try {
+      let response = await fetch(`https://api.opensea.io/user/${address}`, {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'X-API-KEY': process.env.OPENSEA_API_KEY,
+        },
+      })
+      let responseBody = await response.json()
+      if (responseBody.detail) {
+        throw new Error(responseBody.detail)
+      }
+      name = responseBody.username ?? ''
+      osAddressMap[address] = name
+    } catch (err) {
+      // Probably rate limited - return empty sting but don't cache
+      name = ''
+      console.log(err)
+    }
+  }
+
+  return name
+}
+
 module.exports.ensOrAddress = ensOrAddress
+module.exports.getOSName = getOSName
