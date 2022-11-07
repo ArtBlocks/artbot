b8bfadc930a947c41f92dc05154cf4b671cc6554
Philip Mohun <pmohun@users.noreply.github.com>
2022-11-06 22:00:18 -0600

    add additional languages

--- src/Utils/smartBotResponse.js
+++ src/Utils/smartBotResponse.js
@@ -309,11 +309,18 @@ async function smartBotResponse(
   if (
     channelID == CHANNEL_BLOCK_TALK &&
     (msgContentLowercase.includes('thanks') ||
-      msgContentLowercase.includes('thank you') ||
-      msgContentLowercase.includes('gracias') ||
-      msgContentLowercase.includes('domo arigato') ||
-      msgContentLowercase.includes('cheers') ||
-      msgContentLowercase.includes('danke')) &&
+      msgContentLowercase.includes('thank you') || 
+      msgContentLowercase.includes('gracias') || // spanish
+      msgContentLowercase.includes('domo arigato') || // japanese
+      msgContentLowercase.includes('arigato') || // portugese
+      msgContentLowercase.includes('merci') || // french
+      msgContentLowercase.includes('grazie') || // italian
+      msgContentLowercase.includes('obrigada') || // portugese
+      msgContentLowercase.includes('tack') || // swedish
+      msgContentLowercase.includes('cheers') || // australian
+      msgContentLowercase.includes('efcharistó') || // australian
+      msgContentLowercase.includes('blagodaryu vas') || // russian
+      msgContentLowercase.includes('danke')) && // german
     (msgContentLowercase.includes('grant') ||
     msgContentLowercase.includes('grant-san'))
   ) {
