b3fad03985af9e20dfec8cf5cf6e1ce17f3b65c6
grant <grant@artblocks.io>
2023-01-03 10:21:18 -0500

    adding squigs and fixing moments

--- src/Classes/ArtIndexerBot.ts
+++ src/Classes/ArtIndexerBot.ts
@@ -78,6 +78,7 @@ class ArtIndexerBot {
       const heritageStatuses = collectionInfo[1]
       for (let i = 0; i < projects.length; i++) {
         const project = projects[i]
+        if (project.invocations === '0') continue
         console.log(
           `Refreshing project cache for Project ${project.projectId} ${project.name}`
         )
