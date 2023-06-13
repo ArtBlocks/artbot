2e03bc0f8768fae0b8a17235a73c979cecef1bf0
grant <grant@artblocks.io>
2023-06-13 12:12:20 -0500

    tg

--- src/Utils/smartBotResponse.ts
+++ src/Utils/smartBotResponse.ts
@@ -331,6 +331,15 @@ export async function smartBotResponse(
   ) {
     grantThanks++
 
+    const contestAmt = 50
+    if (process.env.CONTEST_ENABLED && grantThanks === contestAmt) {
+      return new EmbedBuilder()
+        .setColor(ARTBOT_GREEN)
+        .setDescription(
+          `Grant thanks YOU ${msgAuthor} for being the ${contestAmt}th person to thank him - you win a free Exstasis piece! Please DM Grant to claim your prize`
+        )
+    }
+
     return new EmbedBuilder()
       .setColor(randomColor())
       .setDescription(
