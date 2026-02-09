a6eb5926c3f1cf0b3f301f777b9413238307833e
grant <grantoesterling1@gmail.com>
2026-02-09 12:23:58 -0600

    trigger

--- codegen.ts
+++ codegen.ts
@@ -4,6 +4,7 @@ const config: CodegenConfig = {
   overwrite: true,
   schema: 'https://data.artblocks.io/v1/graphql',
   documents: ['./src/Data/**/*.graphql'],
+
   generates: {
     'generated/graphql.ts': {
       plugins: [
